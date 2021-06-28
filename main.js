67152622b2e68704258952c27a9fd15103572b5f
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-06-28 18:13:30 -0300

    Made corrections to lyrics in song-section

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
 function contentLoaded() {
 
-  // const nav = document.querySelector('#headerLinks');
-  // const topOfNav = nav.offsetTop;
-  //
-  // function fixNav() {
-  //   console.log(topOfNav);
-  //   console.log(nav.offsetTop);
-  //   if(window.scrollY >= topOfNav) {
-  //     document.body.style.paddingTop = nav.offsetHeight + 'px';
-  //     document.body.classList.add('fixed-nav');
-  //   } else {
-  //     document.body.style.paddingTop = 0;
-  //     document.body.classList.remove('fixed-nav');
-  //   }
-  // }
-  // window.addEventListener('scroll', fixNav);
+
 }
