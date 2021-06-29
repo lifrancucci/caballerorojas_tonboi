2e5a812267dea7fb4c78a5f844e36477a111b4db
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-06-29 17:00:07 -0300

    Add main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 function contentLoaded() {
+  const trackNumbers = document.querySelectorAll('.track-number');
 
+  function hideNumbers() {
+    console.log("it's working");
+  }
+
+  trackNumbers.addEventListener('click', hideNumbers);
 
 }
