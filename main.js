e850362c84eeeb17f866a200fefaa06fe855ba90
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-07-02 21:16:46 -0300

    Add tracks to project. Minor layout corrections

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function contentLoaded() {
 
+  // FIXED NAV
   const nav = document.querySelector('#nav');
   const topOfNav = nav.offsetTop;
 
   function fixNav() {
-    console.log(topOfNav, window.scrollY);
     if(window.scrollY >= topOfNav) {
       document.body.classList.add('fixed-nav');
     } else {
@@ -14,4 +14,28 @@ function contentLoaded() {
 
   window.addEventListener('scroll', fixNav);
 
+  // MEDIA PLAYER
+  // Get elements
+  const player = document.querySelector('.media-player');
+
+  const track1 = player.querySelector('.track1');
+  // const track2 = player.querySelector('.track2');
+  // const track3 = player.querySelector('.track3');
+  // const track4 = player.querySelector('.track4');
+
+  const progress = player.querySelector('.progress');
+  const progressBar = player.querySelector('.progress_filled');
+  const toggle = player.querySelector('.toggle');
+
+  // Build functions
+  function togglePlay() {
+    if(track1.paused){
+      track1.play();
+    } else {
+      track1.pause();
+    }
+  }
+
+  // Hook up Event Listeners
+  toggle.addEventListener('click', togglePlay);
 }
