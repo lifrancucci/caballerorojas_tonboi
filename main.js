f6f4603c4543c62fbfbd7430be4dcb5b84cb2c41
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-07-11 14:02:00 -0300

    Implement interactive track dislay for laptops

--- main.js
+++ main.js
@@ -16,11 +16,37 @@ function contentLoaded() {
 
   // LYRICS
   const trackName = document.querySelectorAll('.track-name');
+  const trackInteractive = document.querySelectorAll('.track-interactive');
+
+  const interactive1 = document.getElementById('interactive1');
+  const interactive2 = document.getElementById('interactive2');
+  const interactive3 = document.getElementById('interactive3');
+  const interactive4 = document.getElementById('interactive4');
 
   function lyricsAppear() {
-    console.log("lyrics appearing!")
+    if(this.closest('.track1')) {
+      interactive1.classList.add('interactive-display');
+    } else {
+      interactive1.classList.remove('interactive-display');
+    }
+    if(this.closest('.track2')) {
+      interactive2.classList.add('interactive-display');
+    } else {
+      interactive2.classList.remove('interactive-display');
+    }
+    if(this.closest('.track3')) {
+      interactive3.classList.add('interactive-display');
+    } else {
+      interactive3.classList.remove('interactive-display');
+    }
+    if(this.closest('.track4')) {
+      interactive4.classList.add('interactive-display');
+    } else {
+      interactive4.classList.remove('interactive-display');
+    }
+
   };
 
-  trackName.forEach(track => track.addEventListener('click', lyricsAppear));
+  trackName.forEach(track => track.addEventListener('mouseenter', lyricsAppear));
 
 }
